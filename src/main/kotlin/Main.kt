99f5b9386ff6fdb7cf09392abf828c3e7d7d4892
greysilly7 <greysilly7@gmail.com>
2025-03-10 08:21:01 -0400

    Stuff ig

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ package org.alpacaindustries
 
 import com.github.juliarn.npclib.api.Npc
 import com.github.juliarn.npclib.api.Position
+import com.github.juliarn.npclib.api.event.InteractNpcEvent
 import com.github.juliarn.npclib.api.profile.Profile
 import com.github.juliarn.npclib.api.profile.ProfileProperty
 import com.github.juliarn.npclib.minestom.MinestomPlatform
@@ -17,21 +18,37 @@ import net.minestom.server.entity.damage.Damage
 import net.minestom.server.event.entity.EntityAttackEvent
 import net.minestom.server.event.entity.EntityDamageEvent
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
+import net.minestom.server.event.player.PlayerChatEvent
+import net.minestom.server.event.server.ServerListPingEvent
 import net.minestom.server.extras.velocity.VelocityProxy
+import net.minestom.server.instance.Instance
 import net.minestom.server.instance.InstanceContainer
+import net.minestom.server.instance.LightingChunk
 import net.minestom.server.instance.anvil.AnvilLoader
+import net.minestom.server.item.ItemStack
 import org.alpacaindustries.config.Config
 import org.alpacaindustries.config.ConfigLoader
+import org.alpacaindustries.config.NPC
+import java.io.ByteArrayOutputStream
+import java.io.DataOutputStream
 
 object MinestormServer {
 
     private val config: Config =
-            runCatching { ConfigLoader.loadConfig() }.getOrElse {
-                throw ExceptionInInitializerError(it)
-            }
+        runCatching { ConfigLoader.loadConfig() }.getOrElse {
+            throw ExceptionInInitializerError(it)
+        }
 
-    lateinit var instanceContainer: InstanceContainer
+    private lateinit var instanceContainer: InstanceContainer
         private set
+
+    private val platform by lazy {
+        MinestomPlatform.minestomNpcPlatformBuilder()
+            .extension(this)
+            .actionController({})
+            .build()
+    }
+
 
     @JvmStatic
     @Throws(IOException::class)
@@ -42,12 +59,14 @@ object MinestormServer {
         // Create the instance
         val instanceManager = MinecraftServer.getInstanceManager()
         instanceContainer = instanceManager.createInstanceContainer()
+
         val worldPath = Path.of("./world")
         if (!Files.exists(worldPath)) {
             Files.createDirectories(worldPath)
         }
-        // instanceContainer.setChunkLoader(AnvilLoader(worldPath.resolve("world")))
-        instanceContainer.setChunkLoader(AnvilLoader("/home/greysilly7/git/lobbyServer/world"))
+        instanceContainer.chunkLoader = AnvilLoader(worldPath)
+        instanceContainer.setChunkSupplier(::LightingChunk)
+
         // Set up event handlers
         setupEventHandlers()
 
@@ -58,6 +77,7 @@ object MinestormServer {
         initializeVelocity()
 
         spawnNPCS()
+        addNPCEvents()
 
         // Start the server
         minecraftServer.start(config.host, config.port)
@@ -72,7 +92,7 @@ object MinestormServer {
             // Use found spawn point or default if not yet available
             player.respawnPoint = Pos(0.0, 17.0, 0.0)
 
-            player.gameMode = GameMode.SURVIVAL
+            player.gameMode = GameMode.ADVENTURE
         }
 
         globalEventHandler.addListener(EntityAttackEvent::class.java) { event ->
@@ -89,57 +109,70 @@ object MinestormServer {
             attacker.sendMessage("${victim.username} §c got hit!")
             event.isCancelled = false
         }
-    }
 
-    private fun spawnNPCS() {
-        val platform =
-                MinestomPlatform.minestomNpcPlatformBuilder()
-                        .extension(this)
-                        .actionController({})
-                        .build()
+        globalEventHandler.addListener(PlayerChatEvent::class.java) { event ->
+            event.isCancelled = true
+        }
 
-        config.npcs.forEach { npc ->
-            platform.newNpcBuilder()
-                    .flag(Npc.LOOK_AT_PLAYER, true)
-                    .flag(Npc.HIT_WHEN_PLAYER_HITS, true)
-                    .flag(Npc.SNEAK_WHEN_PLAYER_SNEAKS, true)
-                    .position(
-                            Position.position(
-                                    npc.x,
-                                    npc.y,
-                                    npc.z,
-                                    npc.yaw,
-                                    npc.pitch,
-                                    instanceContainer.uuid.toString()
-                            )
-                    )
-                    .profile(
-                            Profile.resolved(
-                                    npc.name,
-                                    UUID.randomUUID(),
-                                    setOf(
-                                            ProfileProperty.property(
-                                                    "textures",
-                                                    npc.skinValue,
-                                                    npc.skinSignature
-                                            )
-                                    )
-                            )
-                    )
-                    .buildAndTrack()
+        globalEventHandler.addListener(ServerListPingEvent::class.java) { event ->
+            event.responseData.online = MinecraftServer.getConnectionManager().onlinePlayerCount
+            event.responseData.maxPlayer = (event.responseData.online + 10)
         }
     }
 
-    private fun addNPCEvents() {}
+    private fun spawnNPCS() {
+        config.npcs.forEach { npc ->
+            platform.newNpcBuilder()
+                .flag(Npc.LOOK_AT_PLAYER, true)
+                .flag(Npc.HIT_WHEN_PLAYER_HITS, true)
+                .flag(Npc.SNEAK_WHEN_PLAYER_SNEAKS, true)
+                .position(
+                    Position.position(
+                        npc.x,
+                        npc.y,
+                        npc.z,
+                        npc.yaw,
+                        npc.pitch,
+                        instanceContainer.uuid.toString()
+                    )
+                )
+                .profile(
+                    Profile.resolved(
+                        npc.name,
+                        UUID.randomUUID(),
+                        setOf(
+                            ProfileProperty.property(
+                                "textures",
+                                npc.skinValue,
+                                npc.skinSignature
+                            )
+                        )
+                    )
+                )
+                .buildAndTrack()
+        }
+    }
+
+    private fun addNPCEvents() {
+        platform.eventManager().registerEventHandler(InteractNpcEvent::class.java) {event ->
+            val byteArrayOutputStream = ByteArrayOutputStream()
+            val dataOutputStream = DataOutputStream(byteArrayOutputStream)
+            dataOutputStream.writeUTF("Connect")
+            dataOutputStream.writeUTF((config.npcs.find { npc: NPC -> npc.name == event.npc<Instance, Player, ItemStack, Any>().profile().name() }?.server))
+            val payload = byteArrayOutputStream.toByteArray()
+            // event.player<Player>().sendPluginMessage("bungeecord:main", "Connect " + (config.npcs.find { npc: NPC -> npc.name == event.npc<Instance, Player, ItemStack, Any>().profile().name() }?.server))
+            event.player<Player>().sendPluginMessage("bungeecord:main", payload)
+        }
+    }
 
     private fun addShutdownHook() {
         Runtime.getRuntime()
-                .addShutdownHook(
-                        Thread {
-                            println("Shutting down server...")
-                            MinecraftServer.stopCleanly()
-                        }
-                )
+            .addShutdownHook(
+                Thread {
+                    println("Shutting down server...")
+                    MinecraftServer.stopCleanly()
+                }
+            )
     }
 
     private fun initializeVelocity() {
@@ -147,7 +180,6 @@ object MinestormServer {
             VelocityProxy.enable(config.velocityForwardingSecret)
         }
         if (config.velocitySigned) {
-            throw Exception("Velocity signed forwarding is not yet supported")
             // Additional logic for signed velocity
         }
     }
